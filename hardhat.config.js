42f6e27bc8a20cfd062de6985cf418fa4c9b99ab
Deepak <deepak.rj.dr@gmail.com>
2022-09-21 12:27:56 +0530

    Chagnged hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,8 +8,8 @@ module.exports = {
   },
   networks: {
   matic: {
-    url: 'https://polygon-mumbai.g.alchemy.com/v2/icIDw4cHk2lFJUI1oiXKWO_-uYDno3uq',
-    accounts: ['31557b9f24da29f196bd609b44d84033fc3f3787020f4ee148a72e79d1ae64f8'],
+    url: 'alchemy key',
+    accounts: ['private key'],
   }
 }
 };
